de54b3e966f090296901168ca74f639ca329e00c
Allen Zhang <wr_zhang25@163.com>
2024-06-25 10:34:31 +0800

    fix: delete config-overrides.js

--- config-overrides.js
+++
@@ -1,7 +0,0 @@
-const { override, addBabelPlugins } = require('customize-cra')
-
-module.exports = override(
-  process.env.USE_BABEL_PLUGIN_ISTANBUL && addBabelPlugins(
-    'babel-plugin-istanbul',
-  )
-)
